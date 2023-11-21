d18963e5903012de882c6a60d46de1bad9d86973
minion <100939401+Minion3468@users.noreply.github.com>
2023-11-21 18:57:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
-http
-  .createServer(function (req, res) {
+
+http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h1>AbgfrughbfSD saedasfdsaf 2</h1>" + req.socket.remoteAddress);
   })
